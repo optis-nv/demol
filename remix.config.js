2c22722930556fbd4464a1b89f0e99a3466d1e22
Kenneth Van den Berghe <kenneth.vandenberghe@optis.be>
2023-03-22 16:52:21 +0100

    remix tailwind integration enabled

--- remix.config.js
+++ remix.config.js
@@ -6,6 +6,7 @@ module.exports = {
   future: {
     v2_meta: true,
     v2_routeConvention: true,
+    unstable_tailwind: true,
   },
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };
