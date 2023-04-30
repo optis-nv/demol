26e20eb258f85583c0ff62c82c7c418056350180
Kenneth Van den Berghe <kenneth.vandenberghe@optis.be>
2023-04-30 17:31:41 +0200

    Votes by user for remaining contestants

--- app/models/vote.server.ts
+++ app/models/vote.server.ts
@@ -1,7 +1,9 @@
-import type { Contestant } from "@prisma/client";
+import type { Contestant, Vote } from "@prisma/client";
 import type { User } from "~/auth.server";
 import { prisma } from "~/db.server";
+import { users } from "~/users";
 import { getUserName } from "~/utils";
+import { getContestants } from "./contestant.server";
 
 export const getVoteForEpisode = (episodeId: string, userId: string) => {
   return prisma.vote.findFirst({
@@ -39,4 +41,70 @@ export const castVoteForEpisode = ({
       },
     }),
   ]);
+};
+
+const getVotesRemainingContestants = () => {
+  return prisma.vote.findMany({
+    where: { Contestant: { eliminated: false } },
+  });
+};
+
+export const voteCountsRemainingContestants = async () => {
+  const [votes, contestants] = await Promise.all([
+    getVotesRemainingContestants(),
+    getContestants(),
+  ]);
+  return votes
+    .reduce((acc, vote) => {
+      const contestantVotes = acc.find(
+        (v) => v.contestantId === vote.contestantId
+      );
+      if (!contestantVotes) {
+        return [
+          ...acc,
+          { contestantId: vote.contestantId || "", votes: [vote] },
+        ];
+      }
+      return acc
+        .filter((v) => v.contestantId !== vote.contestantId)
+        .concat({
+          contestantId: vote.contestantId || "",
+          votes: [...contestantVotes.votes, vote],
+        });
+    }, [] as Array<{ contestantId: string; votes: Vote[] }>)
+    .map(({ contestantId, votes }) => {
+      const contestant = contestants.find((c) => c.id === contestantId);
+      if (!contestant) throw new Error("no contestant found");
+      return {
+        contestant: contestant.name,
+        votes: countVotesByUserId(votes),
+      };
+    });
+};
+
+const countVotesByUserId = (votes: Vote[]) => {
+  const votesByUserId = votes
+    .reduce((acc, current) => {
+      const userVotes = acc.find((v) => v.userId === current.userId);
+      if (!userVotes) {
+        return acc.concat({ userId: current.userId, count: 1 });
+      }
+      return acc.map(({ userId, count }) => {
+        if (userId !== current.userId) {
+          return { userId, count };
+        }
+        return { userId, count: count + 1 };
+      });
+    }, [] as Array<{ userId: string; count: number }>)
+    .map(({ count, userId }) => {
+      const user = users.find((u) => u.id === userId);
+      if (!user) throw new Error("user not found");
+      return {
+        count,
+        user: user.name,
+      };
+    });
+  // Don't know how to get toSorted() working with typescript :(
+  votesByUserId.sort((a, b) => b.count - a.count);
+  return votesByUserId.slice(0, 3);
 };
