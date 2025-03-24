d2db0e8aff515c388fab1552b64fb676e7866897
Kenneth Van den Berghe <kenneth.vandenberghe@optis.be>
2025-03-24 09:17:12 +0100

    hide events when not voted

--- app/routes/_app._voting.voted.tsx
+++ app/routes/_app._voting.voted.tsx
@@ -7,6 +7,9 @@ import {
   voteCountsRemainingContestants,
 } from "~/models/vote.server";
 import { requireUser } from "~/auth.server";
+import { getEventLogs } from "~/models/eventLog.server";
+import EventLogs from "./_app._voting/EventLog";
+import { toEvent } from "./_app._voting/route";
 
 export const loader = async ({ request }: LoaderArgs) => {
   const user = await requireUser(request);
@@ -19,49 +22,55 @@ export const loader = async ({ request }: LoaderArgs) => {
   if (!vote) {
     throw redirect("/");
   }
+  const events = await getEventLogs();
   const voteCounts = await voteCountsRemainingContestants();
-  return { vote, voteCounts };
+  return { vote, voteCounts, events };
 };
 
 export default function Voted() {
-  const { vote, voteCounts } = useLoaderData<typeof loader>();
+  const { vote, voteCounts, events } = useLoaderData<typeof loader>();
   return (
-    <div className="flex flex-col">
-      <main>
-        <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
-          Dus jij denkt dat {vote?.Contestant?.name} de mol is?
-        </h1>
-        <h2 className="text-xl leading-tight tracking-tight text-gray-900">
-          Bedankt voor je stem! Nog even afwachten voor we weten of je gelijk
-          hebt.
-        </h2>
-        <img
-          className="mt-4 max-w-sm"
-          src={`/img/${vote?.Contestant?.name}.jpg`}
-          alt={vote?.Contestant?.name}
-        />
-      </main>
-      <aside className="mt-8">
-        <h3 className="text-center text-xl">
-          Hoe hebben we voorlopig gestemd op de resterende kandidaten?
-        </h3>
-        <div className="mt-3 grid grid-cols-3 gap-4 divide-x-2 text-center">
-          {voteCounts.map(({ contestant, votes }) => {
-            return (
-              <div key={contestant}>
-                <h4 className="text-lg font-medium underline">{contestant}</h4>
-                <ul>
-                  {votes.map((vote) => (
-                    <li key={vote.user}>
-                      {vote.user} - {vote.count}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            );
-          })}
-        </div>
-      </aside>
+    <div className="flex flex-col justify-between gap-2 md:flex-row">
+      <div className="flex flex-col">
+        <main>
+          <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
+            Dus jij denkt dat {vote?.Contestant?.name} de mol is?
+          </h1>
+          <h2 className="text-xl leading-tight tracking-tight text-gray-900">
+            Bedankt voor je stem! Nog even afwachten voor we weten of je gelijk
+            hebt.
+          </h2>
+          <img
+            className="mt-4 max-w-sm"
+            src={`/img/${vote?.Contestant?.name}.jpg`}
+            alt={vote?.Contestant?.name}
+          />
+        </main>
+        <aside className="mt-8">
+          <h3 className="text-center text-xl">
+            Hoe hebben we voorlopig gestemd op de resterende kandidaten?
+          </h3>
+          <div className="mt-3 grid grid-cols-3 gap-4 divide-x-2 text-center">
+            {voteCounts.map(({ contestant, votes }) => {
+              return (
+                <div key={contestant}>
+                  <h4 className="text-lg font-medium underline">
+                    {contestant}
+                  </h4>
+                  <ul>
+                    {votes.map((vote) => (
+                      <li key={vote.user}>
+                        {vote.user} - {vote.count}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              );
+            })}
+          </div>
+        </aside>
+      </div>
+      <EventLogs events={events.map(toEvent)} />
     </div>
   );
 }
--- app/routes/_app._voting/route.tsx
+++ app/routes/_app._voting/route.tsx
@@ -1,14 +1,11 @@
 import type { LoaderArgs, SerializeFrom } from "@remix-run/server-runtime";
-import { Outlet, useLoaderData } from "@remix-run/react";
-import { getEventLogs } from "~/models/eventLog.server";
-import EventLogs from "./EventLog";
+import { Outlet } from "@remix-run/react";
 import type { EventLog } from "@prisma/client";
 import { requireUser } from "~/auth.server";
 
 export const loader = async ({ request }: LoaderArgs) => {
   await requireUser(request);
-  const events = await getEventLogs();
-  return { events };
+  return null;
 };
 
 export const toEvent = (event: SerializeFrom<EventLog>) => ({
@@ -17,11 +14,5 @@ export const toEvent = (event: SerializeFrom<EventLog>) => ({
 });
 
 export default function App() {
-  const { events } = useLoaderData<typeof loader>();
-  return (
-    <div className="flex flex-col justify-between gap-2 md:flex-row">
-      <Outlet />
-      <EventLogs events={events.map(toEvent)} />
-    </div>
-  );
+  return <Outlet />;
 }
